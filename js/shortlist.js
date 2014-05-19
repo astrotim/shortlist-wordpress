59bb7794343beb9b3a1dd140b1a5975acbac6c0a
Tim Holt <tim@astronautweb.co>
2014-05-19 11:03:13 +1000

    added namespacing to shortlist methods

--- js/shortlist.js
+++ js/shortlist.js
@@ -1,7 +1,8 @@
-var $ = jQuery; // because we are using WP's noConflict mode jQuery
+/* 
+ * log() by Paul Irish stops console.log() breaking Ajax in IE
+ * http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+ */
 
-// log() by Paul Irish stops console.log() breaking Ajax in IE
-// http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
   log.history = log.history || [];   // store logs to an array for reference
   log.history.push(arguments);
@@ -11,179 +12,202 @@ window.log = function(){
 };
 
 
-/* contents
---------------------------------------------
-
-	1. Update counter with getItemTotal
-	2. Add & remove actions for individual items
-	3. Clear all
-
-*/
-
-// theme directory name
-	var themeDirName = 'shortlist-wordpress';
-
-// path to ajax file
-	var homeURL = window.location.protocol + "//" + window.location.host + "/",
-		filePath = homeURL + 'wp-content/themes/' + themeDirName + '/includes/';
-
-
-/* craete counter for Shortlist nav item
+/* shortlist methods
 ----------------------------------------------------------------------------- */
 
-	var shortlistNavItem = $('.navbar li a[href$="index.php/shortlist/"]'),
-		listCount = $('body').data('count');
-
-	shortlistNavItem.append('&nbsp;(<span class="shortlist-count">0</span>)');
-
-	log(listCount);
-	$('.shortlist-count').html(listCount);
+var shortlist = (function($) { // constructor function
 
 
+  // define object literal container for parameters and methods
+  var method = {};
+
+  // define some parameters
+  // theme directory name
+  var themeDirName = 'shortlist-wordpress';
+
+  // path to ajax file
+  var homeURL = window.location.protocol + "//" + window.location.host + "/",
+      filePath = homeURL + 'wp-content/themes/' + themeDirName + '/includes/';
 
 
-/* Update the shortlist counter
+
+
+  /* append counter to 'shortlist' nav item
+  ----------------------------------------- */
+
+  var shortlistNavItem = $('.navbar li a[href$="/shortlist/"]'),
+      listCount = $('body').data('count');
+
+  shortlistNavItem.append('&nbsp;(<span class="shortlist-count">0</span>)');
+
+  log(listCount);
+  $('.shortlist-count').html(listCount);
+
+
+
+
+  /* Method to update the shortlist counter
+  ----------------------------------------- */
+
+  method.getItemTotal = function() {
+    var counter = $('.shortlist-count'),
+        clearAll = $('.shortlist-clear a');
+
+    $.ajax({
+      type: 'GET',
+      url: filePath + 'shortlist-total.php',
+      success: function(data) {
+        counter.text(data);
+      },
+      error: function() {
+        log('error with getItemTotal function');
+      }
+    });
+  };
+
+
+
+
+  /* Add & remove actions for individual items
+   *
+   * ajax method is wrapped inside itemActions() function
+   *
+   * method has 'button' parameter so jQuery object
+   * can be passed in and run via $(button).on('click'...)
+  -------------------------------------------------------- */
+
+  method.itemActions = function(button) {
+
+    $(button).on('click', function(e) {
+
+      var target    = $(this),
+          item      = target.closest('.item'),
+          itemID    = item.attr('id'),
+          itemAction= target.data('action');
+
+      // test bindings
+      // log(itemAction, itemID);
+
+      $.ajax({
+        type: 'GET',
+        url: filePath + 'shortlist-actions.php',
+        data: 'action=' + itemAction + '&id=' + itemID,
+        success: function() {
+          method.getItemTotal();
+          log(itemAction + ' item ' + itemID);
+        },
+        error: function() {
+          log('error with itemActions function', 'check that "themeDirName" has been correctly set in shortlist.js');
+        }
+      });
+
+      if (itemAction === 'remove') {
+        item.removeClass('selected');
+      } else {
+        item.addClass('selected');
+      }
+
+      e.preventDefault();
+    });
+
+  }; // end fn
+
+
+
+
+
+  /* method to remove items from shortlist page
+  --------------------------------------------- */
+
+  method.pageActions = function () {
+
+    var shortlistPage     = $('.page-template-shortlist-php'),
+        shortlistPageItem = shortlistPage.find('.item'),
+        removeItem        = shortlistPageItem.find('.action');
+
+    removeItem.on('click', function(e) {
+
+      var target = $(this),
+        itemID = target.closest('.item').attr('id');
+
+      $.ajax({
+        type: 'GET',
+        url: filePath + 'shortlist-actions.php',
+        data: 'action=remove&id=' + itemID,
+        success: function() {
+          method.getItemTotal();
+          log('removed item ' + itemID);
+        },
+        error: function() {
+          log('error with removeItem action');
+        }
+      });
+
+      target.closest('.item').remove();
+
+      e.preventDefault();
+    });
+
+  }; // end
+
+
+
+  /* method to empty session with clear button on shortlist page
+  --------------------------------------------------------------- */
+
+  method.clearAll = function () {
+
+    $('.shortlist-clear a').on('click', function(e) {
+
+      $.ajax({
+        type: 'GET',
+        url: filePath + 'shortlist-actions.php',
+        data: 'action=empty',
+        success: function() {
+          method.getItemTotal();
+          // fade and remove selected items
+          $('.items').fadeOut(function() {
+            $(this).remove();
+          });
+        },
+        error: function() {
+          log('error with clearAll action');
+        }
+      });
+
+      e.preventDefault();
+    });
+  }; // end
+
+
+
+  /* make methods accessible
+  -------------------------- */
+  return method;
+
+
+}(jQuery)); // end of shortlist constructor
+
+
+
+
+/* invoke shortlist methods
 ----------------------------------------------------------------------------- */
 
-	function getItemTotal() {
-		var	counter = $('.shortlist-count'),
-			clearAll = $('.shortlist-clear a');
+(function($){
 
-		$.ajax({
-			type: 'GET',
-			url: filePath + 'shortlist-total.php',
-			success: function(data) {
-				counter.text(data);
-			},
-			error: function() {
-				log('error with getItemTotal function');
-			}
-		});
-	};
+  // select items, excluding those on shortlist page
+  var action = $('.item .action:not(.page-template-shortlist-php .item .action)');
+
+  shortlist.getItemTotal();
+  shortlist.itemActions( action );
+  shortlist.pageActions();
+  shortlist.clearAll();
+
+}(jQuery));
 
 
 
 
-/* Add & remove actions for individual items-------------------------------------
-
-	ajax method is wrapped inside shortlistActions() function
-	so it can be called after infinite scroll.
-
-	function has 'button' parameter so jQuery object
-	can be passed in and run via $(button).on('click'...)
-
-	this allows for infinite scroll to apply the ajax method
-	to the new content (moreContent)
-
------------------------------------------------------------------------------ */
-
-	function shortlistActions(button) {
-
-		$(button).on('click', function(e) {
-
-			var target 		= $(this),
-				item 		= target.closest('.item'),
-				itemID 		= item.attr('id'),
-				itemAction 	= target.data('action');
-
-			// test bindings
-			// log(itemAction, itemID);
-
-			$.ajax({
-				type: 'GET',
-				url: filePath + 'shortlist-actions.php',
-				data: 'action=' + itemAction + '&id=' + itemID,
-				success: function() {
-					getItemTotal();
-					log(itemAction + ' item ' + itemID);
-				},
-				error: function() {
-					log('error with shortlistActions function', 'check that "themeDirName" has been correctly set in shortlist.js');
-				}
-			});
-
-			if (itemAction === 'remove') {
-				item.removeClass('selected');
-			} else {
-				item.addClass('selected');
-			}
-
-			e.preventDefault();
-		});
-
-	}; // end fn
-
-
-	// call immediately
-	shortlistActions( $('.item .action:not(.page-template-shortlist-php .item .action)') );
-
-
-
-
-/* remove items from shortlist page
------------------------------------------------------------------------------ */
-
-
-	function shortlistPageActions() {
-
-		var shortlistPage 		= $('.page-template-shortlist-php'),
-			shortlistPageItem 	= shortlistPage.find('.item'),
-			removeItem 			= shortlistPageItem.find('.action');
-
-		removeItem.on('click', function(e) {
-
-			var target = $(this),
-				itemID = target.closest('.item').attr('id');
-
-			$.ajax({
-				type: 'GET',
-				url: filePath + 'shortlist-actions.php',
-				data: 'action=remove&id=' + itemID,
-				success: function() {
-					getItemTotal();
-					log('removed item ' + itemID);
-				},
-				error: function() {
-					log('error with removeItem action');
-				}
-			});
-
-			target.closest('.item').remove();
-
-			e.preventDefault();
-		});
-
-	}; // end
-
-	// call immediately
-	shortlistPageActions();
-
-
-
-
-/* Clear all: empty session with clear button on shortlist page
------------------------------------------------------------------------------ */
-
-	function clearAll() {
-
-		$('.shortlist-clear a').on('click', function(e) {
-
-			$.ajax({
-				type: 'GET',
-				url: filePath + 'shortlist-actions.php',
-				data: 'action=empty',
-				success: function() {
-					getItemTotal();
-				},
-				error: function() {
-					log('error with clearAll action');
-				}
-			});
-
-			e.preventDefault();
-		});
-	} // end
-
-	clearAll();
 
 
